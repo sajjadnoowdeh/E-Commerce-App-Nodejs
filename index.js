03c5b8facd9e83e14829989a552d50689a623a65
sajjad noowdeh <sajjad.noowdeh91@gmail.com>
2022-02-08 23:16:06 +0330

    fix: connenct to server

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express();
 
-
+const moongose = require('mongoose')
+moongose.connect('mongodb+srv://noowdeh:09365679101@cluster0.820i4.mongodb.net/shop?retryWrites=true&w=majority')
+.then(()=>{
+    console.log('DBConnection Successfull!!')
+}).catch((error)=>{
+    console.log(error);
+})
 app.listen(5000,()=>{
     console.log('Backend is running')
 })
