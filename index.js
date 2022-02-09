abac490f7adec2f67ff143903ec4a48989b07328
sajjad noowdeh <sajjad.noowdeh91@gmail.com>
2022-02-09 18:34:53 +0330

    fix: use router register

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express')
 const app = express();
-
 const moongose = require('mongoose')
-moongose.connect('mongodb+srv://noowdeh:09365679101@cluster0.820i4.mongodb.net/shop?retryWrites=true&w=majority')
+const dotenv = require('dotenv')
+const  authRouter = require('./routers/auth')
+dotenv.config();
+
+moongose.connect(process.env.MONGO_URL)
 .then(()=>{
     console.log('DBConnection Successfull!!')
 }).catch((error)=>{
     console.log(error);
 })
-app.listen(5000,()=>{
+
+app.use(express.json())
+
+app.use('/api/auth',authRouter)
+
+
+app.listen(process.env.PORT || 5000,()=>{
     console.log('Backend is running')
 })
