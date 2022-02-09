6f06a48c9307affa5b40214294185a52afe8b919
sajjad noowdeh <sajjad.noowdeh91@gmail.com>
2022-02-09 19:12:28 +0330

    fix: add router login

--- routers/auth.js
+++ routers/auth.js
@@ -1,15 +1,36 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+// REGISTER
 router.post("/register", async (req, res) => {
   const user = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC),
   });
 
   try {
     const saveUser = await user.save();
     res.status(201).json(saveUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// LOGIN
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(401).json("Wrong credentials!!");
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+    const password = hashedPassword.toString(CryptoJS.enc.Utf8);
+    password !== req.body.password &&
+      res.status(401).json("Wrong credentials!!");
+    res.status(200).json(user);
   } catch (err) {
     res.status(500).json(err);
   }
